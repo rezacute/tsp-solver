2b8f3fbcd06ede3f0561b889388dda1bff02c5b7
Syah Riza <rezaqt@gmail.com>
2022-11-24 21:15:29 +0700

    bump

--- backend/utils/csv_loader.rs
+++ backend/utils/csv_loader.rs
@@ -1,5 +1,7 @@
-use polars::prelude::*;
-
+use polars::{prelude::*, export::arrow::array::{new_empty_array, new_null_array}};
+use itertools::Itertools;
+use time;
+use travelling_salesman;
 #[cfg(test)]
 #[path = "csv_loader_tests.rs"]
 mod csv_loader_tests;
@@ -12,6 +14,23 @@ impl CsvLoader{
     pub fn load_sample_file(){
         println!("Loading file");
         let df = CsvReader::from_path("backend/data/data-1.csv").unwrap().finish().unwrap();
+        let out= df.select(["lat","lng"]).unwrap();
+        let mut v = Vec::<(f64,f64)>::new();
+        
+        let ndarray = out.to_ndarray::<Float64Type>().unwrap();
+        let ln = ndarray.rows().into_iter().len() -1;
+        
+
+        for i in 0..ln{
+            let x = ndarray.row(i);
+            let dat :(f64,f64)=(x[0],x[1]);
+            v.push(dat);
+            
+        }
+        println!("{:?}",v);
+        let tour = travelling_salesman::simulated_annealing::solve(&v,time::Duration::seconds(1));
+        println!("Tour distance: {}, route: {:?}", tour.distance, tour.route);
+        
     }
 }
 
