e7874744955f5f470288393fbc6c2437eb1f1ec2
Syah Riza <rezaqt@gmail.com>
2022-11-29 15:50:21 +0700

    add map

--- frontend/src/containers/GeoPoints.tsx
+++ frontend/src/containers/GeoPoints.tsx
@@ -10,37 +10,53 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
-
+import ReactLeafletDriftMarker from "react-leaflet-drift-marker"
+import {
+  MapContainer,
+  TileLayer,
+  useMap,
+  Marker,
+  Popup,
+  Polyline,
+} from "react-leaflet";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import "leaflet/dist/leaflet.css";
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 1000,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 const GeoPointAPI = {
   get: async () => await (await fetch(`/api/tsp`)).json(),
   getResult: async () => await (await fetch(`/api/tsp/solve`)).json(),
 };
 
-const ShowRoute = (props: {
-  points: Array<GeoPoint>;
-  route: Array<number>;
-}) => {
-  let ln = props.points.length;
-  let rs: Array<string> = [];
-  for (let i = 0; i < props.route.length; i++) {
-    let rt = props.route[i];
-    rs.push(props.points[rt].label);
-  }
-  return (
-    <>
-      {rs.map((n, index) => {
-        <span>[{n}]</span>;
-      })}
-      <span>[Start/Finish]</span>
-    </>
-  );
-};
+
+interface Latlong {
+  lat:number,
+  lng:number
+}
 
 export const GeoPoints = () => {
+  const [open, setOpen] = React.useState(false);
+  const [pos,setPos] = React.useState<Latlong>({lat:48.0697249,lng:7.3602374});
+  const [idx,setIdx] = React.useState<number>(0);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const [geoPoints, setGeoPoints] = useState<Array<GeoPoint>>();
   const [solvedPoints, setSolvedPoints] = useState<Array<GeoPoint>>();
   const [processing, setProcessing] = useState<boolean>(false);
   const [tspResult, setTspResult] = useState<TSPResult>();
+  const limeOptions = { color: "red" };
+  
   const solveTSP = async (points: Array<GeoPoint>) => {
     setProcessing(true);
     console.log("->", points.length);
@@ -63,10 +79,28 @@ export const GeoPoints = () => {
 
       setProcessing(false);
     });
+    setTimeout(() => {// updates position every 5 sec
+      if(open){
+        console.log(solvedPoints![idx].label);
+        setPos({lat:solvedPoints![idx].lat,lng:solvedPoints![idx].lng})
+        if(solvedPoints!.length>idx){
+          setIdx(idx+1);
+        }else{
+          setIdx(0);
+        }
+      }
+  }, 3000);
   }, []);
   return (
     <div style={{ display: "flex" }}>
-      <div style={{ flexFlow: "column", textAlign: "left", flex: "50%", marginTop:10 }}>
+      <div
+        style={{
+          flexFlow: "column",
+          textAlign: "left",
+          flex: "50%",
+          marginTop: 10,
+        }}
+      >
         <TableContainer sx={{ maxHeight: 440 }} component={Paper}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table" stickyHeader>
             <TableHead>
@@ -102,9 +136,9 @@ export const GeoPoints = () => {
           flex: "50%",
         }}
       >
-        <div className="Form" style={{marginLeft:20}}>
+        <div className="Form" style={{ marginLeft: 20 }}>
           <span>
-          <button
+            <button
               disabled={processing}
               style={{ height: "40px" }}
               onClick={() => solveTSP(geoPoints || [])}
@@ -113,31 +147,94 @@ export const GeoPoints = () => {
             </button>
           </span>
         </div>
-        <span style={{marginLeft:20}}>result:</span>
+        <span style={{ marginLeft: 20 }}>result:</span>
         <hr></hr>
 
         {tspResult != undefined ? (
           <Container maxWidth="sm">
             <div>
-                <span>
-                  distance: {tspResult.distance} meters
-                </span>
+              <div>
+                <Modal
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="modal-modal-title"
+                  aria-describedby="modal-modal-description"
+                >
+                  <Box sx={style}>
+                    <MapContainer
+                      center={[48.0621758, 7.3561662]}
+                      zoom={16}
+                      scrollWheelZoom={false}
+                      style={{ height: "90vh", width: "100wh" }}
+                    >
+                      <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                      />
+                      {geoPoints?.map((point, index) => (
+                        <>
+                          {index > 0 ? (
+                            <>
+                              <Polyline
+                                pathOptions={limeOptions}
+                                positions={[
+                                  [
+                                    geoPoints[index - 1].lat,
+                                    geoPoints[index - 1].lng,
+                                  ],
+                                  [point.lat, point.lng],
+                                ]}
+                              />
+                            </>
+                          ) : (
+                            <>
+                              <Polyline
+                                pathOptions={limeOptions}
+                                positions={[
+                                  [
+                                    geoPoints[geoPoints.length - 1].lat,
+                                    geoPoints[geoPoints.length - 1].lng,
+                                  ],
+                                  [point.lat, point.lng],
+                                ]}
+                              />
+                            </>
+                          )}
+                          <Marker position={[point.lat, point.lng]}>
+                            <Popup>{point.label}</Popup>
+                          </Marker>
+                        </>
+                      ))}
+                      <ReactLeafletDriftMarker
+            // if position changes, marker will drift its way to new position
+            position={pos}
+            // time in ms that marker will take to reach its destination
+            duration={1000}
+             >
+            <Popup>Hi this is a popup</Popup>
+      
+        </ReactLeafletDriftMarker>
+                    </MapContainer>
+                  </Box>
+                </Modal>
               </div>
-              <hr></hr>
+              <span>distance: {tspResult.distance} meters</span>
+            </div>
+            <hr></hr>
+            <Stack direction="row" spacing={1} maxWidth="500px" flexWrap="wrap">
               <span>route:</span>
-              <Stack direction="row" spacing={1} maxWidth="500px" flexWrap="wrap" >
-                {geoPoints?.map((point, index) => (
-                  <Chip label={point.label} size="small" variant="outlined" />
-                ))}
-              </Stack>
+              <Button onClick={handleOpen}>Open Map</Button>
+            </Stack>
 
-              
-            
+            <Stack direction="row" spacing={1} maxWidth="500px" flexWrap="wrap">
+              {geoPoints?.map((point, index) => (
+                <Chip label={point.label} size="small" variant="outlined" />
+              ))}
+            </Stack>
           </Container>
         ) : (
           <div></div>
         )}
-        
       </div>
     </div>
   );
